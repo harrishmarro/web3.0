84900aeea946e0fc9c8bf0aef19f29581894be4b
harrishmarro <harishmacking11@gmail.com>
2023-05-02 18:11:32 +0530

    added the web content into github

---
+++ client/src/components/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Services= () => {
+  return (
+    <h1>
+        Services
+    
+    </h1>
+  )
+}
+
+export default Services;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,1 @@
+// https://eth-sepolia.g.alchemy.com/v2/xm-ef1N7IhQ7fzDCe6DQTuAy44UFvoOf
